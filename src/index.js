ff912859c8614b79f845a50e0995c303b7a85bff
SergioRuyDev <sergioruyenator@gmail.com>
2022-06-07 15:43:38 -0300

    repository start.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import handler from './handler.js'
 
 const PORT = process.env.PORT || 3000
 
-const server = http.createServer(handler).listen(PORT, () => console.log(`server is running at ${PORT}`))
+const server = http.createServer(handler)
+    .listen(PORT, () => console.log(`server is running at ${PORT}`))
 
 export {
     server
